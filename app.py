1c78ceba27a163a42a3cdb0a85bc40f5156a2ef7
hadadada00 <hadadada0000@gmail.com>
2021-05-18 03:52:34 +0900

    test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from sklearn.linear_model import base
 
 app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
